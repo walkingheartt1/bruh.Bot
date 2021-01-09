fcaaa4c13cf182fd655214bda508c38459e587f0
shakerlu <73892556+shakerlu@users.noreply.github.com>
2021-01-09 23:51:33 +0800

    lession2

--- bot.py
+++ bot.py
@@ -1,3 +1,11 @@
 import discord
+from discord.ext import commands
 
-form discord.ext import commands
+bot = commands.Bot(command_prefix='bruh')
+
+
+@bot.event
+async def on_ready():
+    print("走心機器人 is online")
+
+bot.run('Nzk3MDc3NDYzOTg4OTYxMzEx.X_hOPA.LgBsBuvDtP0s9Y3R7dHSslf6kS4')
