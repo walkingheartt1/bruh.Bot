d2d6260c2d7c984b8f1849a719d12c70fe43686a
shakerlu <73892556+shakerlu@users.noreply.github.com>
2021-01-10 04:14:54 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,22 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='bruh')
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='bruh', intents=intents)
 
 
 @bot.event
 async def on_ready():
     print("走心機器人 is online")
+
+
+@bot.event
+async def on_member_join(member):
+    print(F'{member}來啦!')
+
+
+@bot.event
+async def on_member_remove(member):
+    print(F'{member}走了!')
 
 bot.run('Nzk3MDc3NDYzOTg4OTYxMzEx.X_hOPA.LgBsBuvDtP0s9Y3R7dHSslf6kS4')
